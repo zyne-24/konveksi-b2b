589a5a192f55928151452d2d7fcccd935f36965c
Zyne <zyne@commit.local>
2026-09-13 10:57:06 +0700

    fix: add use client directive to page.tsx to resolve prerender error on Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { SplitText } from "@/components/react-bits/SplitText"
 import { SpotlightCard } from "@/components/react-bits/SpotlightCard"
 import { BeamBackground } from "@/components/react-bits/BeamBackground"
